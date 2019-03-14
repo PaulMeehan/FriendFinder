129d4cdea4b51acdd73f7ae9dd19e4f7cbc932f4
PaulM <pmeehan456@gmail.com>
2019-03-13 22:10:52 -0400

    gets working, push in progress

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        "name": "Aaaaa",
+        "age": 35,
+        "gender": "Male"
+    },
+    {
+        "name": "Bbbbb",
+        "age": 45,
+        "gender": "Female"
+    },
+    {
+        "name": "Cccccc",
+        "age": 55,
+        "gender": "Female"
+    },
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,31 +1,13 @@
-var friends = [
-    {
-        "name": "Aaaaa",
-        "age": 35,
-        "gender": "Male"
-    },
-    {
-        "name": "Bbbbb",
-        "age": 45,
-        "gender": "Female"
-    },
-    {
-        "name": "Cccccc",
-        "age": 55,
-        "gender": "Female"
-    },
-];
-
-console.log(friends);
+const friendsData = require("../data/friends.js");
 
 
 module.exports = function(app) {
-   app.get("/api/friends", function(request, response){
-        console.log("in here");
-        for (var i=0; i < friends.length; i++) {
-            // var newP = "<p>Name: " + friends[i].name + "  Age: " + friends[i].age + "  Gender: " + friends[i].gender + "</p>";
-            var newP = `<p>Name:  ${friends[i].name} Age: ${friends[i].age} Gender: ${friends[i].gender}</p>`;
-            $(".friendList").append(newP);
-        };
-   });
+
+    app.get("/api/friends", function(request, response){
+        response.json(friendsData);
+    });
+
+    app.post ("api/friends", function(request, response) {
+        console.log(json(request));
+    });
 };

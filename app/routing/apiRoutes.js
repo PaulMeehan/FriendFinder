371cdc7652d3ea7f7fc91e567ab4f24959d32aa9
PaulM <pmeehan456@gmail.com>
2019-03-16 23:21:49 -0400

    functioning without images

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,52 @@ module.exports = function(app) {
         response.json(friendsData);
     });
 
+
+    function objectToArray (inObject) {
+        var tempArray = [];
+        tempArray.push(inObject.name);
+        tempArray.push(inObject.image);
+        tempArray.push(inObject.Q1);
+        tempArray.push(inObject.Q2);
+        tempArray.push(inObject.Q3);
+        tempArray.push(inObject.Q4);
+        tempArray.push(inObject.Q5);
+        tempArray.push(inObject.Q6);
+        tempArray.push(inObject.Q7);
+        tempArray.push(inObject.Q8);
+        tempArray.push(inObject.Q9);
+        tempArray.push(inObject.Q10);
+
+        return tempArray;
+    };
+
+
     app.post ("/api/friends", function(request, response) {
-        console.log(request.body);
+        console.log("request.body=" + request.body);
+
+        var newFriend = objectToArray(request.body);
+        var minScore = 41;
+        var bestMatch = -1;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var currentFriend = objectToArray(friendsData[i]);
+            var score = 0;
+            for (var j = 2; j < 11; j++) {
+                score = score + Math.abs(currentFriend[j] - newFriend[j]);
+            };
+
+            if (score < minScore) {
+                bestMatch = i;
+                minScore = score;
+            };
+        };
+
+        console.log("closest match = " + friendsData[bestMatch].name)
+
         friendsData.push(request.body);
-        response.json(true);
+
+
+
+        response.json({ match: true, name: friendsData[bestMatch].name, image: friendsData[bestMatch].image});
     });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,9 +3,7 @@ const path = require("path");
 module.exports = function(app) {
     // Route to the survey page.
     app.get("/survey", function (req, res) {
-        console.log("A");
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-        console.log("B");
     });
 
     // Route to the home page.
